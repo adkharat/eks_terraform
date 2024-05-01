cccb042a5940058240f922432d672914657142a4
ajay-kickdrum <ajay.dilip@kickdrumtech.com>
2024-05-01 19:30:25 +0530

    added storage to jenkins

--- 23_jenkins_server.tf
+++ 23_jenkins_server.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "jenkins-server" {
   subnet_id     = module.vpc.public_subnets[0]
   key_name      = var.ec2_key_name
 
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 30
+    delete_on_termination = true
+  }
+
   # Bootstrap script for Jenkins Ubuntu
   user_data                   = file("Jenkins_server_bootstrap.sh")
   user_data_replace_on_change = true
