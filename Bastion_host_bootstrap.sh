74a4f971d81849c4d9cd12a83d7e83999b51fe9e
ajay-kickdrum <ajay.dilip@kickdrumtech.com>
2024-07-25 08:36:03 +0530

    updated bootstrap script

--- Bastion_host_bootstrap.sh
+++ Bastion_host_bootstrap.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+#Download the latest release
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#Validate the binary (optional)
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#Validate the kubectl binary against the checksum file:
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+#Install kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl   
+#Test to ensure the version you installed is up-to-date:
+kubectl version --client
+
 sudo dnf update -y
 sudo dnf install mariadb105 -y
 
